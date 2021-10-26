b8061dc587f6dfa1067c86bda889e5ed3f14f38e
Stefano <stefano.cipolletta@outlook.it>
2021-10-26 16:01:02 +0200

    Esercizio Lexer - DA AGGIUSTARE

--- 03-First_Lexer/Lexer.java
+++ 03-First_Lexer/Lexer.java
@@ -5,6 +5,7 @@ public class Lexer {
 
     public static int line = 1;
     private char peek = ' ';
+    private String word = "";
 
     private void readch(BufferedReader br) {
         try {
@@ -22,11 +23,53 @@ public class Lexer {
         }
 
         switch (peek) {
+        // ... gestire i casi di ( ) { } + - * / ; , ... //
+
         case '!':
             peek = ' ';
             return Token.not;
 
-        // ... gestire i casi di ( ) { } + - * / ; , ... //
+        case '(':
+            peek = ' ';
+            return Token.lpt;
+
+        case ')':
+            peek = ' ';
+            return Token.rpt;
+
+        case '{':
+            peek = ' ';
+            return Token.lpg;
+
+        case '}':
+            peek = ' ';
+            return Token.rpg;
+
+        case '+':
+            peek = ' ';
+            return Token.plus;
+
+        case '-':
+            peek = ' ';
+            return Token.minus;
+
+        case '*':
+            peek = ' ';
+            return Token.mult;
+
+        case '/':
+            peek = ' ';
+            return Token.div;
+
+        case ';':
+            peek = ' ';
+            return Token.semicolon;
+
+        case ',':
+            peek = ' ';
+            return Token.comma;
+
+        // ... gestire i casi di || < > <= >= == <> ... //
 
         case '&':
             readch(br);
@@ -38,7 +81,50 @@ public class Lexer {
                 return null;
             }
 
-            // ... gestire i casi di || < > <= >= == <> ... //
+        case '|':
+            readch(br);
+            if (peek == '|') {
+                peek = ' ';
+                return Word.or;
+            } else {
+                System.err.println("Erroneous character" + " after | : " + peek);
+                return null;
+            }
+
+        case '=':
+            readch(br);
+            if (peek == '=') {
+                peek = ' ';
+                return Word.eq;
+            } else {
+                System.err.println("Erroneous character" + " after & : " + peek);
+                return null;
+            }
+
+        case '<':
+            readch(br);
+            switch (peek) {
+            case '=':
+                peek = ' ';
+                return Word.le;
+            case '>':
+                peek = ' ';
+                return Word.ne;
+            default:
+                peek = ' ';
+                return Word.lt;
+            }
+
+        case '>':
+            readch(br);
+            switch (peek) {
+            case '=':
+                peek = ' ';
+                return Word.ge;
+            default:
+                peek = ' ';
+                return Word.gt;
+            }
 
         case (char) -1:
             return new Token(Tag.EOF);
@@ -46,11 +132,121 @@ public class Lexer {
         default:
             if (Character.isLetter(peek)) {
 
+                if (peek == 'a') {
+                    readch(br);
+                    if (peek == 's') {
+                        readch(br);
+                        if (peek == 's') {
+                            readch(br);
+                            if (peek == 'i') {
+                                readch(br);
+                                if (peek == 'g') {
+                                    readch(br);
+                                    if (peek == 'n') {
+                                        peek = ' ';
+                                        return Word.assign;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } else if (peek == 't') {
+                    readch(br);
+                    if (peek == 'o') {
+                        peek = ' ';
+                        return Word.to;
+                    }
+                } else if (peek == 'i') {
+                    readch(br);
+                    if (peek == 'f') {
+                        peek = ' ';
+                        return Word.iftok;
+                    }
+                } else if (peek == 'e') {
+                    readch(br);
+                    if (peek == 'l') {
+                        readch(br);
+                        if (peek == 's') {
+                            readch(br);
+                            if (peek == 'e') {
+                                peek = ' ';
+                                return Word.elsetok;
+                            }
+                        }
+                    } else if (peek == 'n') {
+                        readch(br);
+                        if (peek == 'd') {
+                            peek = ' ';
+                            return Word.end;
+                        }
+                    }
+                } else if (peek == 'w') {
+                    readch(br);
+                    if (peek == 'h') {
+                        readch(br);
+                        if (peek == 'i') {
+                            readch(br);
+                            if (peek == 'l') {
+                                readch(br);
+                                if (peek == 'e') {
+                                    peek = ' ';
+                                    return Word.whiletok;
+                                }
+                            }
+                        }
+                    }
+                } else if (peek == 'b') {
+                    readch(br);
+                    if (peek == 'e') {
+                        readch(br);
+                        if (peek == 'g') {
+                            readch(br);
+                            if (peek == 'i') {
+                                readch(br);
+                                if (peek == 'n') {
+                                    peek = ' ';
+                                    return Word.begin;
+                                }
+                            }
+                        }
+                    }
+                } else if (peek == 'p') {
+                    readch(br);
+                    if (peek == 'r') {
+                        readch(br);
+                        if (peek == 'i') {
+                            readch(br);
+                            if (peek == 'n') {
+                                readch(br);
+                                if (peek == 't') {
+                                    peek = ' ';
+                                    return Word.print;
+                                }
+                            }
+                        }
+                    }
+                } else if (peek == 'r') {
+                    readch(br);
+                    if (peek == 'e') {
+                        readch(br);
+                        if (peek == 'a') {
+                            readch(br);
+                            if (peek == 'd') {
+                                peek = ' ';
+                                return Word.read;
+                            }
+                        }
+                    }
+                }
+
                 // ... gestire il caso degli identificatori e delle parole chiave //
+                return null;
 
             } else if (Character.isDigit(peek)) {
 
                 // ... gestire il caso dei numeri ... //
+
+                return new NumberTok((int) peek);
 
             } else {
                 System.err.println("Erroneous character: " + peek);
@@ -61,7 +257,7 @@ public class Lexer {
 
     public static void main(String[] args) {
         Lexer lex = new Lexer();
-        String path = "...path..."; // il percorso del file da leggere
+        String path = "./lexer.txt"; // il percorso del file da leggere
         try {
             BufferedReader br = new BufferedReader(new FileReader(path));
             Token tok;
--- 03-First_Lexer/NumberTok.java
+++ 03-First_Lexer/NumberTok.java
@@ -1,3 +1,13 @@
 public class NumberTok extends Token {
-    
+    public int lexeme = 0;
+
+    public NumberTok(int s) {
+        super(256);
+        lexeme = s;
+    }
+
+    public String toString() {
+        return "<" + tag + ", " + lexeme + ">";
+    }
+
 }
